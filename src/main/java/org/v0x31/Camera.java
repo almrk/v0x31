1a87263509297739c80c65dbd0c2c326ec212697
almrk <199596498+almrk@users.noreply.github.com>
2026-02-25 19:56:18 +0000

    Begin work on camera system

--- src/main/java/org/v0x31/Camera.java
+++ src/main/java/org/v0x31/Camera.java
@@ -1,8 +1,34 @@
 package org.v0x31;
 
-import org.joml.Vector2f;
+import org.joml.Vector3f;
 
 public class Camera {
-    private Vector2f position;
-    private Vector2f direction;
+    // Attributes
+    private Vector3f position;
+    private Vector3f front;
+    private Vector3f up;
+    private Vector3f right;
+    private Vector3f worldUp;
+    // Angles
+    private float yaw;
+    private float pitch;
+    // Options
+    public float speed;
+    public float sensitivity;
+    public float zoom;
+
+    public Camera() {
+        this.position = new Vector3f(0.0f, 0.0f, 0.0f);
+        this.front = new Vector3f(0.0f, 0.0f, 0.0f);
+        this.up = new Vector3f(0.0f, 0.0f, 0.0f);
+        this.right = new Vector3f(0.0f, 0.0f, 0.0f);
+        this.worldUp = new Vector3f(0.0f, 0.0f, 0.0f);
+
+        this.yaw = -90.0f;
+        this.pitch = 0.0f;
+
+        this.speed = 3.0f;
+        this.sensitivity = 0.1f;
+        this.zoom = 70.0f;
+    }
 }
